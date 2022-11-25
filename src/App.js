a8e6970db96d0763e2d3f7139f89a1a7a7ee9570
Abir Hasan <cse.170201063@gmail.com>
2022-11-25 17:49:55 +0600

    section done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Options from "./Components/Option/Options";
 import Picked from "./Components/Picked/Picked";
-
+import Today from "./Components/Today/Today";
+import img from './images/Component 1.png'
 function App() {
   return (
     <div className="max-w-[1560px] mx-auto">
@@ -10,6 +12,9 @@ function App() {
       <Header></Header>
       <Options></Options>
       <Picked></Picked>
+      <img className='my-20 px-10' src={img} alt="" />
+      <Today></Today>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Today/Today.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import Cards from '../Picked/Cards/Cards';
+
+const Today = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+      fetch("fakeApi.json")
+        .then((res) => res.json())
+        .then((data) => setProducts(data));
+    }, []);
+    const six = products.slice(0, 5);
+    console.log(six);
+    return (
+      <div>
+        <div className="">
+          <h2 className="border-dashed font-bold border-b-2 text-xl border-orange-500 p-3">
+            Today's hot deals
+          </h2>
+          <div className="grid grid-cols-5 gap-2">
+            {six.map((one) => (
+              <Cards one={one} key={one._id}></Cards>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Today;
